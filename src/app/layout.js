344a6404c8f86adcf88970f1ce8340727edf110d
Nervonis <lockon2200@naver.com>
2024-01-11 15:42:26 +0900

    init

---
+++ src/app/layout.js
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: '포트폴리오 Next.js',
+  description: '개발자 안병언의 포트폴리오 사이트',
+  keywords: ["포트폴리오", "Next.js"],
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ko">
+      <link rel="icon" type="image/svg+xml" href="favicon.svg" />
+      <body>{children}</body>
+    </html>
+  )
+}
